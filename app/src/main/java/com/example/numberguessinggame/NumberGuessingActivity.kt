38bab4332f78ede03fd356324fc0f77f93cb46b9
User <nika.papitashvili.1@btu.edu.ge>
2021-01-28 22:48:45 +0400

    wrote game code

--- app/src/main/java/com/example/numberguessinggame/NumberGuessingActivity.kt
+++ app/src/main/java/com/example/numberguessinggame/NumberGuessingActivity.kt
@@ -2,10 +2,34 @@ package com.example.numberguessinggame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_number_guessing.*
+import kotlin.random.Random
+
 
 class NumberGuessingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_number_guessing)
+        var randomNum = randomNumber()
+        checkButton.setOnClickListener {
+            val enteredNumber = numberGuessingEditText.text.toString()
+            if (randomNum > Integer.parseInt(enteredNumber)){
+                Toast.makeText(this, "Enter Higher Number", Toast.LENGTH_SHORT).show()
+            }
+            else if (randomNum < Integer.parseInt(enteredNumber)){
+                Toast.makeText(this, "Enter The Lower Number", Toast.LENGTH_SHORT).show()
+            }
+            else if (enteredNumber.isEmpty()){
+                Toast.makeText(this, "Please Fill Field", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            else{
+                Toast.makeText(this, "Great, You Found The Number", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+    fun randomNumber() :Int{
+        return Random.nextInt(1000)
     }
 }

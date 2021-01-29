b2f9f937de25397b0a9a0800c5c30351fbfc1015
Davit Terterashvili <datoterterashvili@gmail.com>
2021-01-29 10:41:35 +0400

    minor changes

--- app/src/main/java/com/example/numberguessinggame/NumberGuessingActivity.kt
+++ app/src/main/java/com/example/numberguessinggame/NumberGuessingActivity.kt
@@ -1,5 +1,6 @@
 package com.example.numberguessinggame
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -9,23 +10,33 @@ import kotlin.random.Random
 
 class NumberGuessingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+//        val user: String? = intent.extras?.getString("user_mail","")
+//        userView.text = user
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_number_guessing)
         var randomNum = randomNumber()
         checkButton.setOnClickListener {
+            val attempts = 0
             val enteredNumber = numberGuessingEditText.text.toString()
             if (randomNum > Integer.parseInt(enteredNumber)){
                 Toast.makeText(this, "Enter Higher Number", Toast.LENGTH_SHORT).show()
+                attempts+1
             }
             else if (randomNum < Integer.parseInt(enteredNumber)){
                 Toast.makeText(this, "Enter The Lower Number", Toast.LENGTH_SHORT).show()
+                attempts+1
             }
             else if (enteredNumber.isEmpty()){
                 Toast.makeText(this, "Please Fill Field", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
             else{
-                Toast.makeText(this, "Great, You Found The Number", Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    this,
+                    "Great, You Found The Number In $attempts Attemps", Toast.LENGTH_SHORT).show()
+                    Intent(this, NumberGuessingActivity::class.java)
+                    startActivity(intent)
+
             }
         }
     }
